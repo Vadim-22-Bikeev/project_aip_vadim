2751b26a94923dbfe3892a09a2dac0a5553d8643
vad13ruz@yandex.ru <vad13ruz@yandex.ru>
2023-12-18 05:27:33 +0300

    Показать статистику

--- input.py
+++ input.py
@@ -1,15 +1,14 @@
 input_data = InlineKeyboardButton(
     text='Введите дату через пробел (год месяц день):',
-    callback_data='data')
+    callback_data='data_way')
 # Создаем объект инлайн-клавиатуры
 keyboard_input = InlineKeyboardMarkup(
     inline_keyboard=[[input_data]])
 # Этот хэндлер будет срабатывать на команду "/start"
 # и отправлять в чат клавиатуру с инлайн-кнопками
-@dp.callback_querty(F.data =='statist')
+@dp.callback_query(F.data == 'statist')
 async def input(callback: CallbackQuery):
-    await callback.answer(
+    await bot.send_message(
+        chat_id=f'{callback.from_user.id}',
         text='Нажмите на кнопку:',
-        reply_markup=keyboard_input)
-if __name__ == '__main__':
-    dp.run_polling(bot)
+        reply_markup = keyboard_input)
